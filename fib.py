6d71efc1272c9355550564994833a44e4055044c
Your Name~ <you@example.com>
2023-11-26 20:22:15 +0900

    update

--- fib.py
+++ fib.py
@@ -3,15 +3,16 @@
 Fibonacci number generator
 When given a position, the function returns the fibonacci at that position in the sequence.
 The zeroth number in the fibonacci sequence is 0. The first number is 1
-Negative numbers should return None
+Negative numbers should return None qq
 """
 def fibonacci(position):
+
  if position < 0:
  return None
  elif position == 0:
  return 0
  elif position == 1:
- return 1
+ return 2
  else:
  return fibonacci(position - 1) + fibonacci(position - 2)
 
